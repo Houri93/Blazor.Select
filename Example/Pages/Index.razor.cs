23d129f6190d6038764d4722006f939ca43b8c4a
Dx xD <en8@live.com>
2022-01-05 09:40:52 +0000

    j

--- Example/Pages/Index.razor.cs
+++ Example/Pages/Index.razor.cs
@@ -2,15 +2,15 @@
 
 public partial class Index
 {
-    private List<string> strings = new();
+    private List<string> values = new();
 
     protected override void OnInitialized()
     {
-        strings.Add("Hello 1");
-        strings.Add("Hello 2");
-        strings.Add("Hello 3");
-        strings.Add("Hello 4");
-        strings.Add("Hello 5");
+        values.Add("Hello 1");
+        values.Add("Hello 2");
+        values.Add("Hello 3");
+        values.Add("Hello 4");
+        values.Add("Hello 5");
 
         base.OnInitialized();
     }
